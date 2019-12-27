4011cd05fffd651e717ee6f0115594bc22431dd4
sunil <suneel.shenoy@gmail.com>
2019-12-26 22:43:12 -0500

    Updates

---
+++ precache-manifest.b72493dc67737098469df3563a5bfa69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f1aaab48e93ea8a7428f74f4853dbc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9a312f47fb1c88a7c35b",
+    "url": "/monsters-rolodex/static/css/main.0a5ccab6.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a312f47fb1c88a7c35b",
+    "url": "/monsters-rolodex/static/js/main.50436163.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
